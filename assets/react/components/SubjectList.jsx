38903feb2dc7b889c83dd5d4ed403ad79aa20ccb
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2026-01-18 09:53:06 +0100

    test deployment

--- assets/react/components/SubjectList.jsx
+++ assets/react/components/SubjectList.jsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
-import { Button } from "./ui/Button";
+import { Button } from "./ui/button";
 
 export default function SubjectList({ onChange }) {
     const [subjects, setSubjects] = useState([]);
