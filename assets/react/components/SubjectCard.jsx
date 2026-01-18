e2f035dc6486ecc57605b55d0da9e70435fc056f
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2026-01-18 21:05:02 +0100

    add logo subject

--- assets/react/components/SubjectCard.jsx
+++ assets/react/components/SubjectCard.jsx
@@ -2,24 +2,29 @@ import React from "react";
 import { CheckIcon } from "@heroicons/react/24/solid"; // icône par défaut
 
 export default function SubjectCard({ subject, selected, onToggle }) {
-  return (
-    <div
-      onClick={onToggle}
-      className={`
+    return (
+        <div
+            onClick={onToggle}
+            className={`
         w-20 h-20 flex flex-col items-center justify-center
         rounded-lg cursor-pointer
         ${selected ? "bg-neutral-200" : "bg-neutral-100"}
         transition-colors duration-200
         relative
       `}
-    >
-      {/* Icône au centre */}
-      <CheckIcon className={`w-6 h-6 ${selected ? "text-black" : "text-black"}`} />
+        >
+            <img
+                src={subject.logo}
+                alt={subject.name}
+                className={`w-8 h-8 ${selected ? "opacity-100" : "opacity-70"}`}
+            />
 
-      {/* Nom du sujet en dessous */}
-      <span className={`mt-2 text-xs font-medium text-center ${selected ? "text-black" : "text-black"}`}>
-        {subject.name}
-      </span>
-    </div>
-  );
+            {/* Nom du sujet en dessous */}
+            <span
+                className={`mt-2 text-xs font-medium text-center ${selected ? "text-black" : "text-black"}`}
+            >
+                {subject.name}
+            </span>
+        </div>
+    );
 }
