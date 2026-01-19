3289fc9a0801db8de848e8c56cfaaeea2eb4425c
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2026-01-19 20:37:38 +0100

    change bg main section to white

--- assets/react/App.jsx
+++ assets/react/App.jsx
@@ -46,7 +46,7 @@ export default function App() {
             {/* Main */}
             <main
                 className="
-                    bg-gray-100
+                    bg-white
                     w-full
                     h-full
                     flex flex-col items-center justify-center
